7c990a03979b148e24c6c7ca2cbe2e53b6cf418c
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-08-10 18:51:20 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,16 @@
+figlet "Acep Gans"
+echo ""
+echo "Sedang Menginstall Bahan Dulu"
+sleep 2
+apt update && apt upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python2 -y
+pip install  --upgrade pip
+pip2 install requests mechanize
+
+pkg install curl
+
+clear
+
+echo "Installing selesai"
